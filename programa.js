ed63b10599c5bd80cab530fc4f3723b0dd1598ef
Ruben Millan <rfmilland@udistrital.edu.co>
2023-03-02 14:27:44 -0500

    version 0.3

---
+++ programa.js
@@ -0,0 +1,19 @@
+// comentario
+
+/* 
+Comentario de 
+varias lineas */
+
+//no es necesario declarar una variable para empezar
+
+ //variables de ambito de función
+
+ var a;
+
+ //Variables de ambito de bloque
+
+ let a;
+
+ //Operaciones: aritméticas, comparación, lógica binaria(AND, OR)
+ 
+ //Estructuras de datos: Arreglos (guardan mas de una variable)
